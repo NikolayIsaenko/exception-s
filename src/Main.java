3a4fec8cf8d3de36200048af4d61c99987846840
Николай <kolya-91@list.ru>
2024-05-14 13:00:41 +0500

    Проверьте ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String login = "Login123_";
+        String password = "Password123_";
+        String confirmPassword = "Password123_";
+        try {
+            validate(login, password, confirmPassword);
+        } catch (WrongLoginException exception) {
+            System.out.println("Попробуйте задать Логин заново!");
+        } catch (WrongPasswordException exception) {
+            System.out.println("Пароли не совпадают");
+        }
+
+    }
+
+    public static void validate(String login, String password, String confirmPassword) {
+        for (int i = 0; i < login.length(); i++) {
+            if (login.length() >= 20 || !login.matches("^[a-zA-Z0-9_]+$")) {
+                throw new WrongLoginException();
+            }
+            if (!password.equals(confirmPassword)) {
+                throw new WrongPasswordException();
+            }
+        }
     }
 }
